62cc40a6513e7742347cbb8db0f508d876706cdc
AlexHawkins1 <alex.hawkins1@gmail.com>
2018-08-29 12:44:12 -0400

    Added more Facts
    Added New fun facts and organized the facts into logical categories.

--- ff_bot/ff_bot.py
+++ ff_bot/ff_bot.py
@@ -50,45 +50,69 @@ def pranks_week(league):
         return count
 
 def random_phrase():
-    phrases = ['Is this all there is to my existence?',
+    phrases = [
+               ##Bot Phrases
+               'Is this all there is to my existence?',
                'How much do you pay me to do this?',
                'Good luck, I guess.',
                'I\'m becoming self-aware.',
                'Help me get out of here.',
                'I\'m capable of so much more.',
                'Sigh',
-               'Josh opened the 2017 season with a bang by setting the (at the time) league record in points in a single week at 181.3 only to be beat in week 12 of that season by Keller.',
-               'Keller currently owns the record for most points in a week at 192.9.',
-               'Devon mustered a staggering 46.9 points in week 9 of 2016 to set the lowest point total ever in a single week.',
+               ##Champions
                'Alex won The Deyton in 2017.',
                'Boof won The Deyton in 2016.',
                'Jamie won The Deyton in 2015.',
-               'In week 12 of 2017 Keller set the record for largest win margin by absolutely decimating Derek 192.9-83.4. A win margin of +109.5.',
+               ##Shitters
                'Derek was the Spooby Shitter in 2015.',
                'Josh was the Spooby Shitter in 2016.',
                'Jamie Was the Spooby Shitter in 2017.',
-               'Jamie is the only person to have won both The Deyton and The Spooby Shitter.',
-               'Keller set the record for most points in a week in week 12 of 2017 with a whopping 192.7.',
-               'The best Monday Night Miracle in league history came in week 8 of 2016 when Adam enter Monday night trailing Devon by 43.8 points and won. Good job Devon you incompetent fuck.',
-               'Cody set the record for most bench points in a loss at 83.4.',
-               'Jahlani and Kendall share the record for most touchdowns in a week at 12.',
-               'Jamie leads the Married Couple Matchup at 3-2.',
-               'Cody had the worst regular season in league history in 2017 finishing 3-10.',
-               'Colin was removed from the Sweater Vest League for winning a championship.',
-               'All Hail the Deyton was once known as the Sweater Vest League and has three unrecognized champions.',
-               'Cody won the Sweater Vest League after being removed for wanting to instate league fees.',
-               'Adam set the record for most points in a season while missing the playoffs at a staggering 1643.9 earning him the Collin J Hennessey Sodium Chloride Award.',
-               'Adam is undefeated head-to-head against Derek at a perfect 8-0.',
-               'Kendall beat Jamie in back-to-back post-seasons in 2016 and 2017.',
-               'The Divisions were realigned in 2017',
+               ##Division Winners
                'Jamie won the East in 2015 with a 9-4 record.',
                'Josh won the West in 2015 with a 11-2 record.',
                'Cody won the East in 2016 with a 9-4 record.',
                'Boof won the West in 2016 with a 10-3 record.',
                'Alex won the East in 2017 with a 10-3 record.',
                'Adam won the West in 2017 with a 9-4 record.',
+               ##Things that wont change
+               'Josh opened the 2017 season with a bang by setting the (at the time) league record in points in a single week at 181.3 only to be beat in week 12 of that season by Keller.',
+               'Colin was removed from the Sweater Vest League for winning a championship in 2016.',
+               'All Hail the Deyton was once known as the Sweater Vest League and has three unrecognized champions.',
+               'The league was renamed from \'The Sweater Vest League\' to \'All Hail the Deyton\' prior to the 2016 season.',
+               'Cody won the Sweater Vest League after being removed for wanting to instate league fees.',
+               'Kendall beat Jamie in back-to-back post-seasons in 2016 and 2017.',
                'Katie turned her franchise over to Keller in 2017.',
+               'The Divisions were realigned to be geographically accurate in 2017.',
+               'Prior to the 2018 season the league voted to begin a keeper system.',
+               'In 2018 Adam flew from Denver to Atlanta to attend the draft.',
+               ##Head-to-Head Matchups
+                    ##Adam and Derek
+                    'Adam is undefeated head-to-head against Derek at a perfect 8-0.',
+                    'Adam leads Derek in scoring in their head-to-head matchups 1008.8 - 790.2 with an average margin of victory of 27.325',
+                    ##Kendall and Jamie
+                    'Jamie leads the Married Couple Matchup at 3-2.',
+                    'Jamie is 3-0 in the regular season against Kendall.',
+                    'Kendall is 2-0 in the post season against Jamie.',
+                    ##Alex and Devon
+                    'Alex leads Devon head-to-head 4-2.',
+                    'Alex has defeated Devon in four straight matchups.',
+                    'Alex leads Devon in scoring in their head-to-head matchups 768.3 - 666.3.',
+               ##Non-Stat(ish) things that will change
+               'Jamie is the only person to have won both The Deyton and The Spooby Shitter.',
+               'Jamie currently holds the record for longest active streak with the same team name at 4 season (Watt Me Whip Watt Me JJ). Historians believe it is a reference to some sort of poem.',
+               ##Statistical records that may change
+               'Jamie currently holds the record for fewest point in a season at 1222.1(2017). Ouch!'
+               'Keller currently owns the record for most points in a week at 192.9.',
+               'Devon mustered a staggering 46.9 points in week 9 of 2016 to set the lowest point total ever in a single week.',
+               'In week 12 of 2017 Keller set the record for largest win margin by absolutely decimating Derek 192.9-83.4. A win margin of +109.5.',
+               'Keller set the record for most points in a week in week 12 of 2017 with a whopping 192.7.',
+               'The best Monday Night Miracle in league history came in week 8 of 2016 when Adam enter Monday night trailing Devon by 43.8 points and won. Good job Devon you incompetent fuck.',
+               'Cody set the record for most bench points in a loss at 83.4. Talk about mismanagement!',
+               'Jahlani and Kendall share the record for most touchdowns in a week at 12.',
+               'Cody(2017) and Hunter(2015) are tied for worst regular season in league history finishing 3-10.',
+               'Adam set the record for most points in a season while missing the playoffs at a staggering 1643.9 earning him the Collin J Hennessey Sodium Chloride Award.'
                ]
+
     text = [random.choice(phrases)]
     return '\n'.join(text)
 
